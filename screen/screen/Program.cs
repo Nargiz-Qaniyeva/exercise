404ccbca08e98631e98905299cf822b5a07ca028
Nargiz-Qaniyeva <tu7lyw21z@code.edu.az>
2022-03-05 18:08:44 +0400

    first commit

---
+++ screen/screen/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace screen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string soz = ("programmist");
+            int count = 0;
+            foreach (char item in soz )
+            {
+                if (item == 'a')
+                {
+                    count++;
+
+                }
+            }
+
+
+            {
+                Console.WriteLine("a reqemlerinin sayi {0}", +count);
+            }
+        }
+    }
+}
